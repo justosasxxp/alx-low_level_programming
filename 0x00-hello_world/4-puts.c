cb253f0e3f4834401cc135175c87ac590e80ee6d
justosasxxp <osahonjeffex@gmail.com>
2022-09-08 17:13:25 -0700

    new commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: Print a string using followed by a new line
+ *Return: 0
+ */
+int main(void)
+
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
