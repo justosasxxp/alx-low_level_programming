43c40332221fec6f43b78733e56dbf79ec16ad8d
justosasxxp <osahonjeffex@gmail.com>
2022-09-21 14:10:04 -0700

    new commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,20 @@
  *@dest: The destination string
  *@src: The source string
  *
- *Return: A pointer to the resulting string dest
+ *Return: A character
  */
 char *_strcat(char *dest, char *src);
 {
-	while (*dest != '\0')
-		destination++;
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
+	int len = 0, i;
 
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = scr[i];
+		len += 1;
+	}
+	dest[len] = '\0';
 	return (dest);
 }
