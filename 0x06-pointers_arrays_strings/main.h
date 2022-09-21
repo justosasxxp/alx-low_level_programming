d4f69dd7724b93ed12dcf7c702ad5226acb52a0b
justosasxxp <osahonjeffex@gmail.com>
2022-09-21 12:54:46 -0700

    new commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,12 +10,7 @@
  *normally you would just use #include <string.h>
  */
 
-#include <stdio.h>
 
-void positive_or_negative(int n);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
