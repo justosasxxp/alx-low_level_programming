199187b6fec7932668aecf9f1c42084abc2f01c5
justosasxxp <osahonjeffex@gmail.com>
2022-10-10 15:56:15 -0700

    new commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ *main - a program that prints name of file it was compiled from
+ *
+ *
+ *
+ *Return: void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
