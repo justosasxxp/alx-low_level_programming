394c1880af869951af9625697146b883e42b327e
justosasxxp <osahonjeffex@gmail.com>
2022-12-14 15:50:27 -0800

    new commit

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ *simple callculation functions
+ *
+ *return "result"
+ */
+
+int add(int x, int y)
+{
+        return(x + y);
+}
+
+
+/**
+ * subtraction lol
+ */
+
+int sub(int x, int y)
+{
+	return(x - y);
+}
+
+
+/**
+ * multiplication
+ */
+
+int mul(int x, int y)
+{
+	return(x * y);
+}
+
+/**
+ * division
+ */
+
+int div(int x, int y)
+{
+	return(x / y);
+}
+
+/**
+ * modulus
+ */
+
+int mod(int x, int y)
+{
+	return(x % y);
+}
+
+
